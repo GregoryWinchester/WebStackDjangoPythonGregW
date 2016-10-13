5b5da25c69f2d905837da8d3331a13442ae6e17a
Gregory Winchester <rollins49@yahoo.com>
2016-10-12 20:44:56 -0600

    chapter 4 complete

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+This is where my projects go.
+
+~Gregory Winchester
+
 # WebStackDjangoPythonGregW
+
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the output from Peaches in Rango")
+    context_dict = {'boldmessage' : "Bunnies and Dinos forever!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'name' : 'Peaches K. Winchester',
+                    'year' : '2016'
+                    }
+    return render(request, 'rango/about.html', context=context_dict)
